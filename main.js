75cdc98c3cdf5b72c51b0ab3d140a1969cf60576
mtfrat <martinfraticelli@gmail.com>
2021-08-11 11:41:51 -0300

    desafio clase 2

---
+++ main.js
@@ -0,0 +1,9 @@
+let texto = prompt("Ingrese hola o chau: ")
+
+if(texto == "hola"){
+    alert("Usted ingreso hola")
+} else if(texto == "chau"){
+    alert("Usted ingreso chau")
+    } else {
+    alert("Usted no ingreso hola ni chau")
+    }
